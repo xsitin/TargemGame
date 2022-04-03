2a2131272d1a8921368fd72a79ef4d2539d64962
xsitin <m9ckom@gmail.com>
2022-04-03 13:32:34 +0500

    first version of attack mechanic

--- Assets/Scripts/Animations/AttackBehaviour.cs
+++ Assets/Scripts/Animations/AttackBehaviour.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Platformer.Core;
+using Platformer.Mechanics;
 using Platformer.Model;
 using UnityEngine;
 
 public class AttackBehaviour : StateMachineBehaviour
 {
+    public int AttackAfterFrame;
+    private int currentFrame;
+
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool("attacking",true);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        currentFrame++;
+        if (currentFrame >= AttackAfterFrame) animator.SetBool(PlayerController.AnimatorObjects.Attacking, true);
+    }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool("attacking",false);
+        animator.SetBool(PlayerController.AnimatorObjects.Attacking, false);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
